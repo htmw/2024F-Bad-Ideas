f0f7f956bc1fbca6de2ab6862681e508da4888e3
Bharadwaj Asireddy <133190663+Bharadwaj2599@users.noreply.github.com>
2024-10-16 20:59:35 -0400

    basic server code

---
+++ server/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify
+import requests
+import os
+
+app = Flask(__name__)
+
+API_KEY = os.environ.get('OPENWEATHER_API_KEY')
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+
+@app.route('/weather', methods=['GET'])
+def get_weather():
+    city = request.args.get('city')
+    if not city:
+        return jsonify({"error": "City parameter is required"}), 400
+
+    params = {
+        'q': city,
+        'appid': API_KEY,
+        'units': 'metric'
+    }
+    response = requests.get(BASE_URL, params=params)
+    
+    if response.status_code == 200:
+        data = response.json()
+        weather = {
+            'city': data['name'],
+            'temperature': data['main']['temp'],
+            'description': data['weather'][0]['description'],
+            'icon': data['weather'][0]['icon']
+        }
+        return jsonify(weather)
+    else:
+        return jsonify({"error": "City not found"}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
